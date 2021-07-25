f74bac699fb6566fae751f6fd717a25ae570e63e
rr- <rr-@sakuya.pl>
2021-07-25 15:55:43 +0200

    cmd/set_desktop: support --relative

--- pyxdotool/commands/set_desktop.py
+++ pyxdotool/commands/set_desktop.py
@@ -10,7 +10,18 @@ class SetDesktopCommand(BaseCommand):
     @classmethod
     def decorate_arg_parser(cls, parser: argparse.ArgumentParser) -> None:
         parser.add_argument("desktop", type=int, help="desktop to switch to")
+        parser.add_argument(
+            "--relative",
+            action="store_true",
+            help=(
+                "use relative movements instead of absolute. This lets you "
+                "move relative to the current desktop."
+            ),
+        )
 
     @classmethod
     def run(cls, ctx: CommandContext) -> None:
-        ctx.xdo.set_current_desktop(ctx.args.desktop)
+        target_desktop = ctx.args.desktop
+        if ctx.args.relative:
+            target_desktop += ctx.xdo.get_current_desktop()
+        ctx.xdo.set_current_desktop(target_desktop)
